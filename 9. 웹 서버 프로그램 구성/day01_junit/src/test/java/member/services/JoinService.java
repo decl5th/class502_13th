14a32e66478f22a2b65ae5fb3ec45051001f4ec2
decl5th <alsgur6624@gmail.com>
2024-06-05 15:06:39 +0900

    TDD chapter 1

--- 9. 웹 서버 프로그램 구성/day01_junit/src/test/java/member/services/JoinService.java
+++ 9. 웹 서버 프로그램 구성/day01_junit/src/test/java/member/services/JoinService.java
@@ -1,11 +1,19 @@
 package member.services;
 
 
+import global.exceptions.ValidationException;
+import global.validators.Validator;
 import member.controllers.RequestJoin;
+import member.validators.JoinValidator;
 
 public class JoinService {
+    // 구성으로 기능 확장
+    private Validator<RequestJoin> validator;
 
+    public JoinService(Validator<RequestJoin> validator) {
+        this.validator = validator;
+    }
     public void process(RequestJoin form) {
-        System.out.println("회원가입");
+        validator.check(form);
     }
 }
