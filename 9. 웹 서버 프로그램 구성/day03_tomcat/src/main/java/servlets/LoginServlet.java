4fbf6ed29dd674650cb6625f8e46393d62e0a92d
decl5th <alsgur6624@gmail.com>
2024-06-11 17:59:43 +0900

    인스턴스 변수 정의 지양에 대한 해설

--- 9. 웹 서버 프로그램 구성/day03_tomcat/src/main/java/servlets/LoginServlet.java
+++ 9. 웹 서버 프로그램 구성/day03_tomcat/src/main/java/servlets/LoginServlet.java
@@ -8,6 +8,8 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class LoginServlet extends HttpServlet {
+    // private int num; 인스턴스 변수 정의 -> 지양 why? 동시성의 문제로 설정한 값이 통제를 벗어나기 때문에
+    // 쓰레드에서 여러개로 접근하다가 음수 나왔던 예제가 대표적인 예
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("로그인!");
