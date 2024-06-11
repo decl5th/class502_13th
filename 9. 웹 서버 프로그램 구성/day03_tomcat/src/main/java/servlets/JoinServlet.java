8369623f76068921e3475c7e4855e88be69df57f
decl5th <alsgur6624@gmail.com>
2024-06-11 16:05:00 +0900

    setcontentType 메서드를 통해 한글 깨지기 않게 문자형 명시

--- 9. 웹 서버 프로그램 구성/day03_tomcat/src/main/java/servlets/JoinServlet.java
+++ 9. 웹 서버 프로그램 구성/day03_tomcat/src/main/java/servlets/JoinServlet.java
@@ -6,10 +6,15 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class JoinServlet extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html"); // content-type는 UTF-8으로 설정해서 한글이 출력되도록 설정
+        //resp.setCharacterEncoding("UTF-8");
+        PrintWriter out = resp.getWriter();
+        out.println("<h1>회원가입</h1>"); // 한글이 깨져서 출력 why? content-type을 알려주지 않았기 때문
 
     }
 
