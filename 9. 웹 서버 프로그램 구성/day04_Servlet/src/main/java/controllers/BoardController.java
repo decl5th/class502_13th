675ace9411f713e1567fe83fc193204b52553687
decl5th <alsgur6624@gmail.com>
2024-06-12 10:50:02 +0900

    filter를 보여주기 위해 board 서블릿 생성

---
+++ 9. 웹 서버 프로그램 구성/day04_Servlet/src/main/java/controllers/BoardController.java
@@ -0,0 +1,10 @@
+package controllers;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet("/board")
+public class BoardController extends HttpServlet {
+
+
+}
