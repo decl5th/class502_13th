79cfb5ba6eda75a018516651d3e711e9e4fecda3
decl5th <alsgur6624@gmail.com>
2024-06-14 20:04:08 +0900

    jstl <c:forEach> 적용한 컨트롤러에서 뷰로 넘어간 예제 해설 주석 달기

--- 9. 웹 서버 프로그램 구성/day05_JSP/src/main/java/board/controllers/BoardListController.java
+++ 9. 웹 서버 프로그램 구성/day05_JSP/src/main/java/board/controllers/BoardListController.java
@@ -13,16 +13,23 @@ import java.io.IOException;
 import java.util.List;
 
 @WebServlet("/board/list/*")
+// *에 어떤 문자열이 오든 그게 게시판의 id가 된다
 public class BoardListController extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+        // model
         BoardInfoService service = new BoardInfoService();
         List<BoardData> items = service.getList();
 
         // 속성을 통해서 view로 넘겨주기 위해 속성을 set을 통해 설정
+        // view에서 쓸 데이터 = 속성
+        // request 객체를 통해서 setAttribute로 설정
         req.setAttribute("items", items);
 
+        // 버퍼를 교체할건데 forward를 통해서 어디서?
+        // request객체에 있는 getRequestDispatcher가지고 가져와서 forwarding 버퍼를 치환하면 됨
+        // jsp파일의 경로를 입력해주면 그 경로로 치환됨 == view를 보여줌
         RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/templates/board/list.jsp");
         rd.forward(req, resp);
     }
--- 9. 웹 서버 프로그램 구성/day05_JSP/src/main/java/board/services/BoardInfoService.java
+++ 9. 웹 서버 프로그램 구성/day05_JSP/src/main/java/board/services/BoardInfoService.java
@@ -7,7 +7,10 @@ import java.util.List;
 import java.util.stream.IntStream;
 
 public class BoardInfoService {
-
+    // 데이터를 가져올 건데 list를 가져오는 로직인데
+    // 10개의 게시글을 db에서 만들었다 가정하고 가져오는 로직
+    // 이런 데이터는 출력이 목적 뷰에서 출력하기 위해서
+    // 속성으로 넘기면 출력
     public List<BoardData> getList() {
         // 게시글 목록 조회 로직 ....
 
