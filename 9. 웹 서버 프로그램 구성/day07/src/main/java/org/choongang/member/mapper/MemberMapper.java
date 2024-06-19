365aadd88a8eaffb22abbc1c93e952fe00ce2ee6
decl5th <alsgur6624@gmail.com>
2024-06-19 15:59:07 +0900

    회원에 대한 DB 정보 등록 및 연결을 위한 로직 작성

--- 9. 웹 서버 프로그램 구성/day07/src/main/java/org/choongang/member/mapper/MemberMapper.java
+++ 9. 웹 서버 프로그램 구성/day07/src/main/java/org/choongang/member/mapper/MemberMapper.java
@@ -1,4 +1,11 @@
 package org.choongang.member.mapper;
 
+import org.choongang.member.entities.Member;
+
 public interface MemberMapper {
+    // 만든 쿼리에 대한 테스트 진행
+    long exist(String email);
+    Member get(String email); // 조회
+    int register(Member member); // 회원 등록
+
 }
