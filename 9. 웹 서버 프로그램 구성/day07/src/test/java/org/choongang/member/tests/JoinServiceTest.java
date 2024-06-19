98abaa52165aad8cabfac830fb415096dba9d117
decl5th <alsgur6624@gmail.com>
2024-06-19 11:41:29 +0900

    서비스 개발을 위한 TDD 스타트

---
+++ 9. 웹 서버 프로그램 구성/day07/src/test/java/org/choongang/member/tests/JoinServiceTest.java
@@ -0,0 +1,16 @@
+package org.choongang.member.tests;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class JoinServiceTest {
+
+    @Test
+    @DisplayName("회원가입 성공시")
+    void successTest() {
+
+    }
+}
