b928bac66d69e02b638458deaa425519371acf44
decl5th <alsgur6624@gmail.com>
2024-06-20 15:02:20 +0900

    이동 이후 컨트롤러와의 연결을 위한 메서드 작성 그리고 예외에 대한 응답코드 설정 및 스크립트 표시

--- 9. 웹 서버 프로그램 구성/day07/src/main/java/org/choongang/member/controllers/JoinController.java
+++ 9. 웹 서버 프로그램 구성/day07/src/main/java/org/choongang/member/controllers/JoinController.java
@@ -6,8 +6,12 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.choongang.global.exceptions.CommonException;
+import org.choongang.member.services.JoinService;
+import org.choongang.member.services.MemberServiceProvider;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet("/member/join")
 public class JoinController extends HttpServlet {
@@ -19,6 +23,17 @@ public class JoinController extends HttpServlet {
 
     @Override
     public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //여기에 서비스 연동(추가)
+        try {
+            JoinService service = MemberServiceProvider.getInstance().joinService();
+            service.process(req); // req가 정의되어 있지 않으니 서비스 내부에서 정의해서 변환시키겠음
+        } catch (CommonException e) {
+            // alert 형태로 자바스크립트를 띄울 건데 태그 안에서 contentType이 text/html이 되어야 함
+            resp.setContentType("text/html; charset=UTF-8");
+            resp.setStatus(e.getStatus());
+            PrintWriter out = resp.getWriter();
+            out.printf("<script>alert('%s');</script>", e.getMessage());
 
+        }
     }
 }
