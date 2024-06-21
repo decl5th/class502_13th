01cbf5862c1ec17211ec8dec87f032ad620e0d12
decl5th <alsgur6624@gmail.com>
2024-06-21 10:52:15 +0900

    파일 업로드에 대한 셋업 jsp와 컨트롤러 생성

---
+++ 9. 웹 서버 프로그램 구성/day07/src/main/java/org/choongang/file/controllers/FileUploadController.java
@@ -0,0 +1,22 @@
+package org.choongang.file.controllers;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/file/upload")
+public class FileUploadController extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
