a647bf9c630d5786424aadddf608846b860c7980
decl5th <alsgur6624@gmail.com>
2024-06-21 14:50:11 +0900

    파일을 웹에서 출력하지 않고 파일로 내보내서 내부에 저장하도록 메서드 작성

--- 9. 웹 서버 프로그램 구성/day07/src/main/java/org/choongang/file/controllers/FileDownloadController.java
+++ 9. 웹 서버 프로그램 구성/day07/src/main/java/org/choongang/file/controllers/FileDownloadController.java
@@ -6,18 +6,28 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
+import java.nio.file.Files;
 
 @WebServlet("/file/download")
 public class FileDownloadController extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setHeader("Content-Disposition", "attachment; filename=downloadtest.txt");
+        File file = new File("D:/uploads/coding.png");
+        String contentType = Files.probeContentType(file.toPath()); // image/png
+        String fileName = new String(file.getName().getBytes(), "ISO8859_1"); // coding.png | 윈도우즈에서 한글 깨짐 문제 발생 , 인코딩 UTF-8 -> CPC949, EUC-KR, ISO8859_1
+        // 파일명을 byte로 바꿔서 인식하도록 설정
 
-        PrintWriter out = resp.getWriter();
-        out.print("ABC");
-        out.print("DEF");
+        resp.setHeader("Content-Disposition", "attachment; filename=" + fileName);
+        resp.setContentType(contentType);
+        resp.setHeader("Cache-Control", "must-revalidate");
+        resp.setIntHeader("Expires", 0);
+        resp.setContentLengthLong(file.length());
 
+        // byte 형태로 오기 때문에 outputStream
+        try (BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file))){
+            OutputStream out = resp.getOutputStream();
+            out.write(bis.readAllBytes());
+        }
     }
 }
