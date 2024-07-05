6e011cb81edb7ca5ace39dd810a60b9118f416d1
decl5th <alsgur6624@gmail.com>
2024-07-05 16:06:19 +0900

    @Autowired를 쓰지 않고 DI를 할 수 있는 방법으로 생성자 주입 DI방식 소개 생성자 매개변수를 통한 DI 방식 중에서도 3가지 방식 매개변수들의 정의 혹은 @Nonnull 혹은 final 상수 방식

--- 11. 프레임워크 프로그래밍/day01/src/main/java/exam01/member/services/JoinService.java
+++ 11. 프레임워크 프로그래밍/day01/src/main/java/exam01/member/services/JoinService.java
@@ -4,6 +4,8 @@ import exam01.member.controllers.RequestJoin;
 import exam01.member.dao.MemberDao;
 import exam01.member.entities.Member;
 import exam01.member.validators.JoinValidator;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -11,21 +13,28 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
 
 @Service
+@RequiredArgsConstructor
 public class JoinService {
 
-    @Autowired
+    //@Autowired
     private JoinValidator validator;
 
-    @Autowired
+    //@Autowired
+    @NonNull
+    // @RequiredArgsConstructor를 통한 @Nonnull 방식과 final을 이용한 상수 정의로도
+    // 생성자 매개변수 DI가 가능
     private MemberDao memberDao;
 
     // 의존 관계 - 없으면 객체 생성 X
+    // 생성자 매개변수를 통한 DI
     /*
+    하지만 아래와 같은 방법은 일일이 다 매개변수를 정의해야하는 번거로움이 존재
     public JoinService(JoinValidator validator, MemberDao memberDao) {
         this.validator = validator;
         this.memberDao = memberDao;
     }
-    */
+     */
+
 
     /*
     // 연관 관계 - 없어도 객체는 생성 된다.
