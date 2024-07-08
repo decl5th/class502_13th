bb6db2970b5d03addc1dba891dc0e8c9c20825ba
decl5th <alsgur6624@gmail.com>
2024-07-08 21:14:57 +0900

    excludeFilters에 직접 클래스명을 기입하여 배제하도록 설정

--- 11. 프레임워크 프로그래밍/day02/src/main/java/config/AppCtx.java
+++ 11. 프레임워크 프로그래밍/day02/src/main/java/config/AppCtx.java
@@ -1,12 +1,17 @@
 package config;
 
-import global.annotation.ManualBean;
+import member.dao.MemberDao;
+import member.validators.JoinValidator;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
 @Configuration
+/*
 @ComponentScan(basePackages="member", excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = ManualBean.class)) // ManualBean이 포함되어 있는 자동 스캔에 해당하는 클래스는 객체를 만들지 않고 배제
+
+ */
+@ComponentScan(basePackages = "member", excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {MemberDao.class, JoinValidator.class}))
 public class AppCtx {
 
 }
