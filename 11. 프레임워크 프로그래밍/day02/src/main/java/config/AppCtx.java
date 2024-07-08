1d3c75bef220ed9f8d94b9756b4c5a072e61bcd3
decl5th <alsgur6624@gmail.com>
2024-07-08 21:10:38 +0900

    excludeFilters에 ManualBean 명명

--- 11. 프레임워크 프로그래밍/day02/src/main/java/config/AppCtx.java
+++ 11. 프레임워크 프로그래밍/day02/src/main/java/config/AppCtx.java
@@ -1,10 +1,12 @@
 package config;
 
+import global.annotation.ManualBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 
 @Configuration
-@ComponentScan("member")
+@ComponentScan(basePackages="member", excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = ManualBean.class)) // ManualBean이 포함되어 있는 자동 스캔에 해당하는 클래스는 객체를 만들지 않고 배제
 public class AppCtx {
 
 }
