5e9237d8af7bf58f1da430cbd3eac65890274f92
decl5th <alsgur6624@gmail.com>
2024-07-08 21:12:35 +0900

    MemberDao에 @ManualBean을 붙여 배제하도록 설정 => 테스트 오류 why? 객체 생성이 되지 않기 때문에

--- 11. 프레임워크 프로그래밍/day02/src/main/java/member/dao/MemberDao.java
+++ 11. 프레임워크 프로그래밍/day02/src/main/java/member/dao/MemberDao.java
@@ -1,5 +1,6 @@
 package member.dao;
 
+import global.annotation.ManualBean;
 import member.entities.Member;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +9,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@ManualBean
 @Repository // DAO는 DB 접속과 주로 연결되다 보니 레포지토리 애너테이션을 붙여줌
 public class MemberDao { // memberDao - 빈 이름
 
