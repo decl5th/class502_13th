9ccdf07a2845a5319cefed6961cd22158f2125bb
decl5th <alsgur6624@gmail.com>
2024-07-08 21:32:23 +0900

    excludeFilters에 정규표현식과 ant패턴을 이용하여 배제하도록 설정하는 방법

--- 11. 프레임워크 프로그래밍/day02/src/main/java/config/AppCtx.java
+++ 11. 프레임워크 프로그래밍/day02/src/main/java/config/AppCtx.java
@@ -1,17 +1,22 @@
 package config;
 
-import member.dao.MemberDao;
-import member.validators.JoinValidator;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
 @Configuration
 /*
-@ComponentScan(basePackages="member", excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = ManualBean.class)) // ManualBean이 포함되어 있는 자동 스캔에 해당하는 클래스는 객체를 만들지 않고 배제
+@ComponentScan(basePackages="member", excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = ManualBean.class))
+// ManualBean이 포함되어 있는 자동 스캔에 해당하는 클래스는 객체를 만들지 않고 배제
+ */
+/*
+@ComponentScan(basePackages = "member", excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {MemberDao.class, JoinValidator.class}))
 
  */
-@ComponentScan(basePackages = "member", excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {MemberDao.class, JoinValidator.class}))
+/*
+@ComponentScan(basePackages = "member", excludeFilters = @ComponentScan.Filter(type = FilterType.REGEX, pattern = "member\\.*\\w*Dao"))
+ */
+@ComponentScan(basePackages = "member", excludeFilters = @ComponentScan.Filter(type =FilterType.ASPECTJ, pattern = "member..*Dao"))
 public class AppCtx {
 
 }
