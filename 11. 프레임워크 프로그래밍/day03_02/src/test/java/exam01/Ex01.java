11d347b2a3efb6d9c9e9874949b9b44d12a1e541
decl5th <alsgur6624@gmail.com>
2024-07-09 16:02:49 +0900

    JdbcTemplate을 이용한 query( ) 람다식을 이용한 코드 축소...

--- 11. 프레임워크 프로그래밍/day03_02/src/test/java/exam01/Ex01.java
+++ 11. 프레임워크 프로그래밍/day03_02/src/test/java/exam01/Ex01.java
@@ -1,12 +1,18 @@
 package exam01;
 
 import config.AppCtx;
+import member.Member;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = AppCtx.class)
@@ -19,8 +25,60 @@ public class Ex01 {
     void test1() {
         String sql = "INSERT INTO MEMBER (SEQ, EMAIL, PASSWORD, USER_NAME) " +
                     "VALUES (SEQ_MEMBER.NEXTVAL, ?, ?, ?)"; // -> PreparedStatement
-        int result = jdbcTemplate.update(sql, "user01@test.org", "123456", "user01");
+        int result = jdbcTemplate.update(sql, "user03@test.org", "123456", "user03");
         System.out.println(result);
+    }
 
+    @Test
+    void test2() {
+        List<Member> members = jdbcTemplate.query("SELECT * FROM MEMBER", this::mapper); // 메서드 참조 변수
+                    /*
+                    (rs, num) -> Member.builder()
+                    .seq(rs.getLong("SEQ"))
+                    .email(rs.getString("EMAIL"))
+                    .password(rs.getString("PASSWORD"))
+                    .userName(rs.getString("USER_NAME"))
+                    .regDt(rs.getTimestamp("REG_DT").toLocalDateTime())
+                    .build());
+                    */
+
+        members.forEach(System.out::println);
+    }
+
+    @Test
+    void test3() {
+        String email = "user01@test.org";
+        try {
+            Member member = jdbcTemplate.queryForObject("SELECT * FROM MEMBER WHERE EMAIL = ?", this::mapper, email);
+                /*
+                (rs, num) -> Member.builder()
+
+                .seq(rs.getLong("SEQ"))
+                .email(rs.getString("EMAIL"))
+                .password(rs.getString("PASSWORD"))
+                .userName(rs.getString("USER_NAME"))
+                .regDt(rs.getTimestamp("REG_DT").toLocalDateTime())
+                .build()
+                 */
+            System.out.println(member);
+        } catch (Exception e) {
+            System.out.println("없음");
+        }
+    }
+
+    @Test
+    void test4() {
+        int total = jdbcTemplate.queryForObject("SELECT COUNT(*) FROM MEMBER", Integer.class);
+        System.out.println(total);
+    }
+
+    private Member mapper(ResultSet rs, int num) throws SQLException {
+        return Member.builder()
+                .seq(rs.getLong("SEQ"))
+                .email(rs.getString("EMAIL"))
+                .password(rs.getString("PASSWORD"))
+                .userName(rs.getString("USER_NAME"))
+                .regDt(rs.getTimestamp("REG_DT").toLocalDateTime())
+                .build();
     }
 }
