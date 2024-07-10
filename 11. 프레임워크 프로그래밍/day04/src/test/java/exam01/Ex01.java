ba3794d33f12e780b5bba9e84c1a4cb4ca49ee61
decl5th <alsgur6624@gmail.com>
2024-07-10 09:55:44 +0900

    spring data jdbc 의존성 추가

--- 11. 프레임워크 프로그래밍/day04/src/test/java/exam01/Ex01.java
+++ 11. 프레임워크 프로그래밍/day04/src/test/java/exam01/Ex01.java
@@ -6,13 +6,17 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+@Transactional
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = AppCtx.class)
 public class Ex01 {
@@ -23,17 +27,19 @@ public class Ex01 {
     @Test
     void test1() {
         // 이 때 PreparedStatementCreator는 함수형 인터페이스이기 때문에 람다식으로 줄여 쓰는게 가능하다
+        KeyHolder keyHolder = new GeneratedKeyHolder();
         int result = jdbcTemplate.update(con -> {
                 String sql = "INSERT INTO MEMBER(SEQ, EMAIL, PASSWORD, USER_NAME) values(SEQ_MEMBER.NEXTVAL, ?,?,?)";
 
-                PreparedStatement pstmt = con.prepareStatement(sql);
-                pstmt.setString(1, "user04@test.org");
+                PreparedStatement pstmt = con.prepareStatement(sql, new String[] {"SEQ"});
+                pstmt.setString(1, "user05@test.org");
                 pstmt.setString(2, "12345678");
-                pstmt.setString(3, "user04");
+                pstmt.setString(3, "user05");
 
                 return pstmt;
-        });
+        }, keyHolder);
 
         System.out.println("result = " + result);
+        Number key = keyHolder.getKey();
     }
 }
