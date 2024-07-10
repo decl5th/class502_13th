5649cb45a4103d2adb47dcaf97b8d3f575d7963d
decl5th <alsgur6624@gmail.com>
2024-07-10 16:03:35 +0900

    스프링 컨테이너의 특징 servlet 기본 객체를 담고 있다

--- 11. 프레임워크 프로그래밍/day04/src/main/java/member/controllers/MemberController.java
+++ 11. 프레임워크 프로그래밍/day04/src/main/java/member/controllers/MemberController.java
@@ -1,7 +1,14 @@
 package member.controllers;
 
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -10,6 +17,13 @@ public class MemberController {
     @GetMapping("/member/join")
     public String join(@RequestParam(name="name", defaultValue = "기본값") String name) {
         System.out.println("name = " + name);
+        return "member/join";
+    }
+
+    @PostMapping("/member/join")
+    public String joinPs(RequestJoin form, HttpServletRequest request, HttpServletResponse response, HttpSession session, Model model, Errors errors) {
+        System.out.println(form);
+
         return "member/join";
     }
 }
