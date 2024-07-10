176c9652e4d2d3af1e1a292bdbac25d99ed495d6
decl5th <alsgur6624@gmail.com>
2024-07-10 15:31:31 +0900

    @RequestParam 특징에 대해서 확인 required = false 혹은 defalutValue로 에러 처리

--- 11. 프레임워크 프로그래밍/day04/src/main/java/member/controllers/MemberController.java
+++ 11. 프레임워크 프로그래밍/day04/src/main/java/member/controllers/MemberController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class MemberController {
 
     @GetMapping("/member/join")
-    public String join(@RequestParam(name="name", required = false) String name) {
+    public String join(@RequestParam(name="name", defaultValue = "기본값") String name) {
         System.out.println("name = " + name);
         return "member/join";
     }
