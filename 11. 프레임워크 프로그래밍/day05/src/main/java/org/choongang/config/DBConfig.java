aa3d78ad592c6f1685cd306b51e13ec37709c16e
decl5th <alsgur6624@gmail.com>
2024-07-11 10:48:37 +0900

    커넥션 풀 설정

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/config/DBConfig.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/config/DBConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class DBConfig {
 
-    @Bean
+    @Bean(destroyMethod = "close")
     public DataSource dataSource() {
         DataSource ds = new DataSource();
         /* 연결 설정 B */
@@ -16,7 +16,13 @@ public class DBConfig {
         ds.setUsername("Spring");
         ds.setPassword("oracle");
         /* 연결 설정 D */
+
         /* 커넥션 풀 설정 B */
+        ds.setInitialSize(2);
+        ds.setMaxActive(10);
+        ds.setTestWhileIdle(true);
+        ds.setMinEvictableIdleTimeMillis(1000 * 60); // 기본값이라 따로 추가 안해도 됨
+        ds.setTimeBetweenEvictionRunsMillis(1000 * 5); // 기본값이라 따로 추가 안해도 됨
         /* 커넥션 풀 설정 D */
     }
 }
