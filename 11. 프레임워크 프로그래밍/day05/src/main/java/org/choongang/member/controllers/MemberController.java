ebc677dabb7b9151482a0bbb0a8d9213bf198ed8
decl5th <alsgur6624@gmail.com>
2024-07-12 09:20:11 +0900

    params & logger

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/controllers/MemberController.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/controllers/MemberController.java
@@ -1,6 +1,8 @@
 package org.choongang.member.controllers;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,10 +15,19 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/member")
 public class MemberController {
 
+    private final Logger log = LoggerFactory.getLogger(MemberController.class);
+
     @GetMapping("/join")
+    //@RequestMapping(path = "/member/join", method = RequestMethod.GET)
+    public String join1() {
+        System.out.println("mode 없음");
+        return "member/join";
+    }
+
+    @GetMapping(path = "/join", params = {"mode=join"})
     //@RequestMapping(path="/member/join", method = {RequestMethod.GET, RequestMethod.POST})
     public String join() {
-
+        System.out.println("mode=join");
         return "member/join";
     }
 
