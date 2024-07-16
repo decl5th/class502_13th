60ae703f574b484e46e6dabdb4f76ab31420b8a4
decl5th <alsgur6624@gmail.com>
2024-07-16 10:53:50 +0900

    질문 1&2에 대한 답이 step3이 반영되지 않음

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/survey/controllers/SurveyController.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/survey/controllers/SurveyController.java
@@ -2,6 +2,7 @@ package org.choongang.survey.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,17 +11,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class SurveyController {
 
     @GetMapping("/step1")
-    public String step1() {
+    public String step1(@ModelAttribute RequestSurvey form) {
         return "survey/step1";
     }
 
     @PostMapping("/step2")
-    public String step2() {
+    public String step2(RequestSurvey form) {
         return "survey/step2";
     }
 
     @PostMapping("/step3")
-    public String step3() {
+    public String step3(RequestSurvey form) {
         return "survey/step3";
     }
 }
---
+++ 11. 프레임워크 프로그래밍/day05/src/main/webapp/WEB-INF/templates/survey/step3.jsp
@@ -0,0 +1,6 @@
+<%@ page contentType="text/html; charset=UTF-8" %>
+<%@ taglib prefix="c" uri="jakarta.tags.core" %>
+질문1 : ${requestSurvey.q1}<br>
+질문1 : ${requestSurvey.q2}<br>
+질문1 : ${requestSurvey.q3}<br>
+질문1 : ${requestSurvey.q4}
