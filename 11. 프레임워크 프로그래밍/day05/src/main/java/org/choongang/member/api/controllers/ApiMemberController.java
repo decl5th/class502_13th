5b2ac79a98a1c649cd24e755c8888fc166324a92
decl5th <alsgur6624@gmail.com>
2024-07-17 12:32:00 +0900

    List 형태로 멤버 생성 및 출력

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/api/controllers/ApiMemberController.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/api/controllers/ApiMemberController.java
@@ -1,13 +1,20 @@
 package org.choongang.member.api.controllers;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.choongang.member.entities.Member;
 import org.choongang.member.mappers.MemberMapper;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.IntStream;
+
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/member") // 앞에 api가 붙어있는 주소
@@ -24,9 +31,27 @@ public class ApiMemberController {
         return member;
     }
 
-    @GetMapping("/test")
+    @GetMapping("/list")
+    public List<Member> list() {
+       List<Member> members = IntStream.rangeClosed(1, 10)
+               .mapToObj(i -> Member.builder()
+                       .email("user" + i + "@test.org")
+                       .password("12345678")
+                       .userName("user" + i)
+                       .regDt(LocalDateTime.now())
+                       .build())
+               .toList();
+       return members;
+    }
+
+    @GetMapping(path="/test", produces = "text/html;charset=UTF-8")
     public String test() {
         // Content-Type: text/plain
         return "안녕하세요";
+    }
+
+    @GetMapping("/test2")
+    public void test2() {
+      log.info("test2.....");
     }
 }
