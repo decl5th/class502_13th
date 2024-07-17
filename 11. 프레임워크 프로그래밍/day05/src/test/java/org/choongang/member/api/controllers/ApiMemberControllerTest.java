0ead90c3f5408934102d286043acdfc3b868333d
decl5th <alsgur6624@gmail.com>
2024-07-17 14:59:03 +0900

    Content-Type: application/x-www-form-urlencoded (기본값)으로 출력 테스트

--- 11. 프레임워크 프로그래밍/day05/src/test/java/org/choongang/member/api/controllers/ApiMemberControllerTest.java
+++ 11. 프레임워크 프로그래밍/day05/src/test/java/org/choongang/member/api/controllers/ApiMemberControllerTest.java
@@ -9,10 +9,13 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 @SpringJUnitWebConfig
 @ContextConfiguration(classes = MvcConfig.class)
-
 public class ApiMemberControllerTest {
 
     private MockMvc mockMvc;
@@ -20,13 +23,27 @@ public class ApiMemberControllerTest {
     @Autowired
     private ApiMemberController controller;
 
+    /*
+    @Autowired
+    private WebApplicationContext ctx;
+    */
+
     @BeforeEach
     void init() {
         mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+        //mockMvc = MockMvcBuilders.webAppContextSetup(ctx).build();
     }
 
     @Test
     void test1() throws Exception {
-
+        // Content-Type: application/x-www-form-urlencoded (기본값)
+        // 이름=값 & 이름=값...
+        mockMvc.perform(
+                post("/api/member")
+                .param("email", "user99@test.org")
+                .param("password", "12345678")
+                .param("confirmPassword", "12345678")
+                .param("userName", "user99"))
+                .andDo(print());
     }
 }
