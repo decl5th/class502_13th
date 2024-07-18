1a417d754b3a82a071f6c8821d062a956cdbe71a
decl5th <alsgur6624@gmail.com>
2024-07-18 14:25:10 +0900

    RestTemplate 기능 확인을 위한 jsonplaceholder 와의 연동

--- 11. 프레임워크 프로그래밍/day05/src/test/java/tests/Ex01.java
+++ 11. 프레임워크 프로그래밍/day05/src/test/java/tests/Ex01.java
@@ -1,29 +1,39 @@
 package tests;
 
 import org.choongang.config.MvcConfig;
+import org.choongang.exam.PostData;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
-import org.springframework.web.util.UriComponents;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
 
 @SpringJUnitWebConfig
-@ContextConfiguration(classes = MvcConfig.class)
+@ContextConfiguration(classes= MvcConfig.class)
 public class Ex01 {
-
     @Test
     void test1() {
         URI url = UriComponentsBuilder.fromUriString("https://www.naver.com")
                 .path("/news/{0}")
-                .queryParam("tq","v1")
+                .queryParam("t1", "v1")
                 .queryParam("t2", "v2")
                 .queryParam("t3", "한글")
                 .queryParam("t4", "aa{1}")
                 .fragment("hash")
                 .encode()
-                .build();
+                .build("AAAA", "BBBB");
+
         System.out.println(url);
+
+    }
+
+    @Test
+    void test2() {
+        RestTemplate restTemplate = new RestTemplate();
+        PostData data = restTemplate.getForObject("https://jsonplaceholder.typicode.com/posts/1", PostData.class);
+        System.out.println(data);
     }
 }
