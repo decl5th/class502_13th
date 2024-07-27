09dfec30f64f4d954872c03235c4c4c3601f66b9
decl5th <alsgur6624@gmail.com>
2024-07-27 16:50:30 +0900

    @IdClass를 이용한 복합키 생성

---
+++ 11. 스프링 부트/day02/src/main/java/org/choongang/board/entities/BoardView.java
@@ -0,0 +1,24 @@
+package org.choongang.board.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
+import lombok.Data;
+
+@Data
+@Entity
+@IdClass(BoardViewId.class)
+public class BoardView {
+    // view 카운트를 정할 수 있는 Entity
+    // 복합키를 생성할 클래스
+
+    @Id
+    private Long seq;
+    @Id
+    @Column(name = "_uid") // uid는 오라클 예약어 이미 있어서 컬럼 속성을 이용해서 이름을 바꿔줌
+    private int uid;
+    // 위 2개를 이용해서 복합키 생성
+
+    //@Id들을 묶어주는 ID클래스 생성 필요
+}
