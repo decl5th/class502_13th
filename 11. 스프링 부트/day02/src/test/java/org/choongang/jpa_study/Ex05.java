bd33dee794817440529e23f28fc9f528ca6ab6ac
decl5th <alsgur6624@gmail.com>
2024-07-29 19:56:14 +0900

    여러개의 데이터를 한꺼번에 가공 List<Member> 형태로 테스트2에서 확인
    그외 업데이트와 삭제 메서드 기능 테스트

--- 11. 스프링 부트/day02/src/test/java/org/choongang/jpa_study/Ex05.java
+++ 11. 스프링 부트/day02/src/test/java/org/choongang/jpa_study/Ex05.java
@@ -9,6 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
+import java.util.List;
+import java.util.stream.IntStream;
+
 @SpringBootTest
 @TestPropertySource(properties = "spring.profiles.active=test")
 public class Ex05 {
@@ -18,6 +21,17 @@ public class Ex05 {
 
     @BeforeEach
     void init() {
+        List<Member> members = IntStream.rangeClosed(1, 10)
+                // 정수를 멤버 객체 형태로 변환
+                .mapToObj(i -> Member.builder()
+                        .email("user" + i + "@test.org")
+                        .password("12345678")
+                        .userName("사용자" + i)
+                        .authority(Authority.USER)
+                        .build()).toList(); // 10명의 회원 데이터를 List형태로 가공
+
+        memberRepository.saveAllAndFlush(members);
+        /*
         for (int i = 1; i < 10; i++) {
 
         Member member = Member.builder()
@@ -33,7 +47,7 @@ public class Ex05 {
         }
 
         memberRepository.flush(); // DB에 영구 반영
-
+         */
     }
 
     @Test
@@ -41,7 +55,20 @@ public class Ex05 {
         Member member = memberRepository.findById(1L).orElse(null);
         System.out.println(member);
 
+        member.setUserName("(수정)사용자01");
+        memberRepository.save(member);
+        //memberRepository.flush();
+
         Member member2 = memberRepository.findById(1L).orElse(null);
         System.out.println(member2);
+
+        memberRepository.delete(member2);
+        memberRepository.flush();
+    }
+
+    @Test
+    void test2() {
+        List<Member> members = memberRepository.findAll();
+        members.forEach(System.out::println);
     }
 }
