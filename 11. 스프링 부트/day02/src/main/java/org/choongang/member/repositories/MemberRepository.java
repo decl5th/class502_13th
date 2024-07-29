2672dd345b169e4566331168af1c37d0701b7b1d
decl5th <alsgur6624@gmail.com>
2024-07-29 21:13:56 +0900

    쿼리메서드 - 내가 원하는 데이터만 조회하고 싶을 때 => 조건문 기능 테스트 조건이 늘어날 때 코드가 길어지는 단점 확인

--- 11. 스프링 부트/day02/src/main/java/org/choongang/member/repositories/MemberRepository.java
+++ 11. 스프링 부트/day02/src/main/java/org/choongang/member/repositories/MemberRepository.java
@@ -3,6 +3,14 @@ package org.choongang.member.repositories;
 import org.choongang.member.entities.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
+import java.util.List;
+import java.util.Optional;
 
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    //Optional<Member> findByEmail(String email);
+    // email로 회원 조회하는 쿼리 메서드, null 값도 조회하거나 처리할 경우 옵셔널로 감싸는 형태로도 가능
+    Member findByEmail(String email);
+
+    List<Member> findByEmailContainingAndUserNameContainingOrderByCreatedAtDesc(String key1, String key2);
+    // 쿼리메서드의 단점: 조건이 조금만 늘어나도 코드가 길어진다
 }
---
+++ 11. 스프링 부트/day02/src/test/java/org/choongang/jpa_study/Ex06.java
@@ -0,0 +1,47 @@
+package org.choongang.jpa_study;
+
+import org.choongang.member.constants.Authority;
+import org.choongang.member.entities.Member;
+import org.choongang.member.repositories.MemberRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+@SpringBootTest
+@TestPropertySource(properties = "spring.profiles.active=test")
+public class Ex06 {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @BeforeEach
+    void init() {
+        List<Member> members = IntStream.rangeClosed(1, 10)
+                // 정수를 멤버 객체 형태로 변환
+                .mapToObj(i -> Member.builder()
+                        .email("user" + i + "@test.org")
+                        .password("12345678")
+                        .userName("사용자" + i)
+                        .authority(Authority.USER)
+                        .build()).toList(); // 10명의 회원 데이터를 List형태로 가공
+
+        memberRepository.saveAllAndFlush(members);
+    }
+
+    @Test
+    void test1() {
+        Member member = memberRepository.findByEmail("user2@test.org");
+        System.out.println(member);
+    }
+
+    @Test
+    void test2(){
+        List<Member> members = memberRepository.findByEmailContainingAndUserNameContainingOrderByCreatedAtDesc("ser", "용");
+        members.forEach(System.out::println);
+    }
+}
